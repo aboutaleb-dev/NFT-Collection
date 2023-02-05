c3f7d55f34f02236905950af9042ff47015ddf31
coder-developer-a <aboutaleb.dev@gmail.com>
2023-02-05 17:37:52 +0330

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xcb6B46Ba4e320E5F95Fb77FAB3103515D250407f"
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
